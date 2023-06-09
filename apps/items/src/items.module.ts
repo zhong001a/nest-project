8254fa0d93dde07a1cd7d54e237c59506f812c39
watcharin001 <watcharin.nuengkaew@gmail.com>
2023-06-09 10:52:01 +0700

    set up project

--- apps/items/src/items.module.ts
+++ apps/items/src/items.module.ts
@@ -20,8 +20,8 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
         PORT: Joi.number().required(),
-        // AUTH_HOST: Joi.string().required(),
-        // AUTH_PORT: Joi.number().required(),
+        AUTH_HOST: Joi.string().required(),
+        AUTH_PORT: Joi.number().required(),
 
       }),
     }),
